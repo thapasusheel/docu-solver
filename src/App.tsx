378aaf30a0036f3c57743bd291e95324149f0bb9
thapasusheel <sushilthapa9844242743@gmail.com>
2024-05-11 07:45:33 +0545

    Initialize project using Vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center text-7xl bg-black text-white h-screen font-semibold tracking-wider">
+      DocuSolver
+    </div>
+  );
+};
+
+export default App;
