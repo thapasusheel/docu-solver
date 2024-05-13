bde7a3175f4321a94d8e12ad9b33b1e593b26246
thapasusheel <sushilthapa9844242743@gmail.com>
2024-05-13 11:11:48 +0545

    Newsletter Section of Landing Page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero/Hero";
 import Footer from "@/components/Footer";
 import FAQ from "@/components/FAQ";
+import NewsLetter from "@/components/NewsLetter";
 
 const LandingPage = () => {
   return (
@@ -10,6 +11,7 @@ const LandingPage = () => {
       <Layout className="px-64">
         <Header />
         <Hero />
+        <NewsLetter />
         <FAQ />
       </Layout>
       <Footer />
