81951a73b40d25a76f7642dc8d6240fa702ca4b8
thapasusheel <sushilthapa9844242743@gmail.com>
2024-05-13 08:50:37 +0545

    Footer Section of the Landing Page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,17 @@
 import Layout from "../../layout/Layout";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer";
 
 const LandingPage = () => {
   return (
-    <Layout className="px-64">
-      <Header />
-      <Hero />
-    </Layout>
+    <>
+      <Layout className="px-64">
+        <Header />
+        <Hero />
+      </Layout>
+      <Footer />
+    </>
   );
 };
 
