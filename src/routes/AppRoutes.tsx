f4c03e3d5fd9a5a68d215d6abea12644a1ecd9e4
thapasusheel <sushilthapa9844242743@gmail.com>
2024-05-13 09:12:38 +0545

    Author Page of DocuSolver

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 
+import AuthorPage from "@/pages/AuthorPage";
+
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/author" element={<AuthorPage />} />
       </Routes>
     </Router>
   );
